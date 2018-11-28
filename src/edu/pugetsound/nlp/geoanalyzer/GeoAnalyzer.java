52f18c14060e0f56060c33804187076c6e2a9804
BDague <bd77778@gmail.com>
2018-11-28 10:18:09 -0800

    Updated the Geoanalyzer to take advanatage of a Knowledgebase Controller

--- src/edu/pugetsound/nlp/geoanalyzer/GeoAnalyzer.java
+++ src/edu/pugetsound/nlp/geoanalyzer/GeoAnalyzer.java
@@ -1,17 +1,23 @@
 package edu.pugetsound.nlp.geoanalyzer;
 
 import edu.pugetsound.mathcs.nlp.architecture_nlp.features.TextAnalyzer;
+import edu.pugetsound.mathcs.nlp.kb.KBController;
 import edu.pugetsound.mathcs.nlp.lang.Conversation;
 import edu.pugetsound.mathcs.nlp.lang.Utterance;
 public class GeoAnalyzer {
 	
-	public GeoAnalyzer() {};
-	
-	public static Utterance geoAnalyze(String sentence) {
-			
-		TextAnalyzer textAnalyzer = new TextAnalyzer(null);
+	private TextAnalyzer analyzer;
+	private KBController knowledge;
+
+	public GeoAnalyzer(String directory) {
+		this.knowledge = new KBController(directory);
+		this.analyzer = new TextAnalyzer(this.knowledge);
 		
-		Utterance parsed = textAnalyzer.analyze(sentence, new Conversation());
+	};
+	
+	public Utterance geoAnalyze(String sentence) {
+		
+		Utterance parsed = this.analyzer.analyze(sentence, new Conversation());
 		
 		return parsed;
 	}
--- test/nlp425/geo_analyzer.java
+++ test/nlp425/geo_analyzer.java
@@ -16,9 +16,22 @@ import edu.pugetsound.nlp.geoanalyzer.GeoAnalyzer;
  *
  */
 public class geo_analyzer {
-
+	
+	
 	@Test
 	public void EnsureAnalyzeReturnsUtterance() {
-		assert(GeoAnalyzer.geoAnalyze("This is a sentance") instanceof Utterance);
+		GeoAnalyzer geo_test = new GeoAnalyzer(".");
+		assert(geo_test.geoAnalyze("This is a sentance") instanceof Utterance);
+	}
+	
+	@Test
+	public void TestAnalysisForExceptions() {
+		GeoAnalyzer geo_test = new GeoAnalyzer(".");
+		String sentence1 = "The girl kicked the ball";
+		String sentence2 = "The whale slept";
+		String sentence3 = "I like jell-o";
+		assert(geo_test.geoAnalyze(sentence1) instanceof Utterance);
+		assert(geo_test.geoAnalyze(sentence2) instanceof Utterance);
+		assert(geo_test.geoAnalyze(sentence3) instanceof Utterance);
 	}
 }
